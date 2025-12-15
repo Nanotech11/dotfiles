10d3b3087411f6d3e7328a2601932d87d4b1e945
Nanotech11 <nanotech11acc@gmail.com>
2025-12-14 21:02:19 -0700

    Updating nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,59 @@
+vim.cmd.colorscheme('sorbet')
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 
+-- QoL
+vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
-vim.o.softtabstop = 4
-vim.o.tabstop = 4 
-vim.o.shiftwidth = 4
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+-- Indents
+vim.o.autoindent = true
 vim.o.expandtab = true
 vim.o.smarttab = true
-vim.o.swapfile = false
-vim.o.termguicolors = true
-vim.o.autoindent = true
-vim.o.cursorline = true
+vim.o.smartindent = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 4 
+
+-- Search
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.smartcase = true
+
+-- Visuals
 vim.o.ruler = true
-vim.o.mouse = 'vn'
 vim.o.showmatch = true
+vim.o.termguicolors = true
 vim.o.colorcolumn = '121'
 vim.o.signcolumn = 'yes'
 vim.o.winborder = 'rounded'
 
+-- Files
+vim.o.autoread = true
+vim.o.autowrite = false
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.undodir = vim.fn.expand('~/.vim/undodir')
+
+-- Behavior
+vim.o.autochdir = false
+vim.o.hidden = true
+vim.o.modifiable = true
+vim.o.encoding = 'UTF-8'
+vim.o.mouse = 'a'
+
+-- Keymap
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('i', 'jj', '<C-[>')
+vim.keymap.set('i', 'jk', '<C-[>')
 
 vim.pack.add({
     { src = 'https://github.com/nvim-lua/plenary.nvim' },
@@ -35,7 +64,7 @@ vim.pack.add({
     { src = 'https://github.com/neovim/nvim-lspconfig' },
 })
 
-vim.lsp.enable({ 'ruff' })
+vim.lsp.enable({ 'pyright', 'lua_ls' })
 
 require("nvim-tree").setup()
 
