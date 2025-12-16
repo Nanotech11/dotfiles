4677d1ecbf7f23be41f71ad388150ff9233f26a7
Nanotech11 <nanotech11acc@gmail.com>
2025-12-16 00:25:45 -0700

    working on nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,3 @@
-vim.cmd.colorscheme('sorbet')
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
@@ -32,6 +30,7 @@ vim.o.ruler = true
 vim.o.showmatch = true
 vim.o.termguicolors = true
 vim.o.colorcolumn = '121'
+vim.o.matchtime = 1
 vim.o.signcolumn = 'yes'
 vim.o.winborder = 'rounded'
 
@@ -50,8 +49,8 @@ vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.encoding = 'UTF-8'
 vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
 vim.o.foldmethod = 'expr'
 vim.o.mouse = 'a'
 
@@ -63,7 +62,15 @@ vim.pack.add({
     { src = 'https://github.com/stevearc/oil.nvim' },
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/Saghen/blink.cmp',                version = 'v1.8.0' },
+    { src = 'https://github.com/windwp/nvim-autopairs' },
+    { src = 'https://github.com/stevearc/conform.nvim' },
+    { src = 'https://github.com/catppuccin/nvim' },
+    { src = 'https://github.com/navarasu/onedark.nvim' },
+    { src = 'https://github.com/folke/tokyonight.nvim' },
+    { src = 'https://github.com/Mofiqul/vscode.nvim' },
 })
+vim.cmd.colorscheme('catppuccin')
 ---@diagnostic disable-next-line: missing-fields
 require('nvim-treesitter.configs').setup({
     ensure_installed = {
@@ -73,14 +80,18 @@ require('nvim-treesitter.configs').setup({
         'java',
         'rust',
         'go',
-        'zig'
+        'zig',
     },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
 })
-require('nvim-tree').setup()
+require('nvim-autopairs').setup({
+    disable_filetype = { 'TelescopePrompt', 'vim' },
+    map_cr = true,
+    check_ts = true,
+})
 
 -- LSP
 vim.lsp.enable({
@@ -88,7 +99,10 @@ vim.lsp.enable({
     'pyright',
     'ruff',
     'clangd',
-
+    'java_language_server',
+    'rust_analyzer',
+    'gopls',
+    'zls',
 })
 vim.lsp.config('lua_ls', {
     settings = {
@@ -102,14 +116,64 @@ vim.lsp.config('lua_ls', {
         }
     }
 })
-vim.lsp.inlay_hint.enable(true)
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = vim.api.nvim_create_augroup('RuffAutoOrganize', { clear = true }),
+    pattern = '*.py',
+    callback = function()
+        vim.lsp.buf.code_action({
+            ---@diagnostic disable-next-line: missing-fields
+            context = {
+                only = { 'source.organizeImports' },
+            },
+            apply = true,
+        })
+    end,
+})
+vim.lsp.config('ruff', {
+    init_options = {
+        settings = {
+            configuration = 'C:\\Users\\Noah\\AppData\\Roaming\\ruff\ruff.toml',
+            configurationPreference = 'filesystemFirst',
+        }
+    },
+})
+vim.diagnostic.config({
+    float = {
+        focusable = false,
+        source = true,
+    },
+})
+vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
+    callback = function()
+        vim.diagnostic.open_float(nil, {
+            scope = 'line',
+            close_events = { 'CursorMoved', 'InsertEnter', 'WinLeave', 'BufLeave' },
+        })
+    end,
+})
+require('blink.cmp').setup({
+    signature = {
+        enabled = true,
+    },
+    completion = {
+        documentation = {
+            auto_show = true,
+        },
+    },
+    keymap = {
+        ['<Down>'] = { 'hide', 'fallback' },
+        ['<Up>'] = { 'hide', 'fallback' },
+        ['<Esc>'] = { 'hide', 'fallback' },
+    }
+})
 
 -- Keymap
 vim.keymap.set('i', 'jk', '<C-[>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 local tele_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', tele_builtin.find_files)
